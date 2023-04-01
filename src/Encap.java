42b19e7ef37e48444cd8955c3db6c58186f9cd65
saketkarn <saketkarn38@gmail.com>
2023-04-01 23:07:56 +0530

    First commit

---
+++ src/Encap.java
@@ -0,0 +1,40 @@
+class Student{
+    private int roll;
+    private String name;
+    private String city;
+        public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
+
+public class Encap {
+    public static void main(String[] args){
+        Student obj=new Student();
+        obj.setCity("Patna");
+        obj.setRoll(1);
+        obj.setName("Saket");
+        System.out.println(obj.getName());
+        System.out.println(obj.getRoll());
+        System.out.println(obj.getCity());
+    }
+}
