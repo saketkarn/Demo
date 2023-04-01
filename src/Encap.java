0df957f60291400a68935fab93d0bb203bae31de
saketkarn <saketkarn38@gmail.com>
2023-04-01 23:32:26 +0530

    Encapsulation

--- src/Encap.java
+++ src/Encap.java
@@ -6,34 +6,42 @@ class Student{
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
+//    public void setRoll(int roll) {
+//        this.roll = roll;
+//    }
+
+    public Student(String name, String city, int roll){
+            this.name=name;
+            this.city=city;
+            this.roll=roll;
     }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+
 
     public int getRoll() {
         return roll;
     }
 
-    public void setRoll(int roll) {
-        this.roll = roll;
-    }
+
 }
 
 public class Encap {
     public static void main(String[] args){
-        Student obj=new Student();
+        Student obj=new Student("Saket", "Patna", 1);
         System.out.println("Learning Encapsulation :- ");
-        obj.setCity("Patna");
-        obj.setRoll(1);
-        obj.setName("Saket");
+//        obj.setCity("Patna");
+//        obj.setRoll(1);
+//        obj.setName("Saket");
         System.out.println(obj.getName());
         System.out.println(obj.getRoll());
         System.out.println(obj.getCity());
